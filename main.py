a6a5488bf4a399967c7f9951ff6a1974d53065f7
RynAgain <85427950+RynAgain@users.noreply.github.com>
2022-09-25 01:25:00 -0500

    Starting project

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+import selenium
+
+
+#This program will webscrape the Austin trash collection website and tell me what bins need to go out.
+#vist https://www.austintexas.gov/myschedule
+#input address from config file
+#get next pickup date
+#get the bins
+#set to booleans
+#make lights change
+#if thursday run
+#display on LCD info and who's turn it is to take the trash to curb.
---
+++ rpiAPI.py
@@ -0,0 +1,3 @@
+#A file for speeding up the programing of the Raspberry pi.
+# I am so sick of writing these functions over and over.
+import random
