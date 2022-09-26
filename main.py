43963eb28b219879d9c0e5dd3eeca1cd61b1117c
RynAgain <85427950+RynAgain@users.noreply.github.com>
2022-09-25 21:29:04 -0500

    Saving
    added new structure to my code.  began laying the foundation for organizing it

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+#IMPORTS AND OTHER FILES
 import config #personal info keys
 import rpiAPI #my own raspberry pi programing API
-from time import sleep
+from time import sleep #import the sleep function
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+
+#VARIABLES
+
+#drivers below need to be moved to the main code section
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 driver.get('https://www.austintexas.gov/myschedule') #opens the starting webpage
 
-def find_addbox(): #this will be a function for finding the 
+def find_addybox(): #this will be a function for finding the address box and filling it in with the address
     pass
 
 def find_nextevent():
     pass
+
+#code for finding the address input, will eventually be moved to a function
 sleep(3)
 addybox = driver.find_element(By.ID, 'row-input-0')
 sleep(2)
@@ -23,8 +30,19 @@ sleep(2)
 addybox.send_keys(Keys.ENTER)
 sleep(1)
 addybox.send_keys(Keys.ENTER)
+sleep(3)
+
+#at this point the trash day should be loaded on screen the next step is to extract the data
+
+next_date = driver.find_element(By.)
+
 sleep(100)
-driver.close()
+driver.close() # kills the web browser 
+
+#MAIN BODY OF CODE GOES HERE.
+
+
+#PSU CODE, TRACKER, LINKS, STUFF
 #This program will webscrape the Austin trash collection website and tell me what bins need to go out.
 #vist https://www.austintexas.gov/myschedule
 #webdrivers https://github.com/SergeyPirogov/webdriver_manager
